974774208889fef6898fdd687e1767385c824658
Tyler Fahey <twfahey1@gmail.com>
2020-10-06 07:53:28 -0500

    Test adding path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ export DOCKSAL_VERSION="${DOCKSAL_VERSION:-master}"
 sudo mkdir -p /usr/local/bin &&
 	sudo curl -fsSL "https://raw.githubusercontent.com/docksal/docksal/${DOCKSAL_VERSION}/bin/fin?r=${RANDOM}" -o /usr/local/bin/fin &&
 	sudo chmod +x /usr/local/bin/fin &&
-	fin update
+  echo "/usr/local/bin/" >> $GITHUB_PATH
