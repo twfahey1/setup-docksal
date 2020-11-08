143169a45247ad71d966f4181b3147dac9c811bc
Tyler Fahey <twfahey1@gmail.com>
2020-11-08 10:48:13 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export DOCKSAL_VERSION="${DOCKSAL_VERSION:-master}"
 
-sudo mkdir -p /usr/local/bin &&
-	sudo curl -fsSL "https://raw.githubusercontent.com/docksal/docksal/${DOCKSAL_VERSION}/bin/fin?r=${RANDOM}" -o /usr/local/bin/fin &&
-	sudo chmod +x /usr/local/bin/fin &&
-  echo "/usr/local/bin/" >> $GITHUB_PATH
+sudo mkdir -p $HOME/docksal &&
+	sudo curl -fsSL "https://raw.githubusercontent.com/docksal/docksal/${DOCKSAL_VERSION}/bin/fin?r=${RANDOM}" -o $HOME/docksal/fin &&
+	sudo chmod +x $HOME/docksal/fin &&
+  echo "$HOME/docksal/" >> $GITHUB_PATH
